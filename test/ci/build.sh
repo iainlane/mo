c31c5c51913e5c175edcb0edeefba215a635f2b6
Iain Lane <iain@orangesquash.org.uk>
2016-09-04 20:43:41 +0100

    travis: Do a meson build too

--- test/ci/build.sh
+++ test/ci/build.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
 set -e
-set -x
+set -v
 
-NOCONFIGURE=1 ./autogen.sh
-./configure --disable-silent-rules --enable-werror CC=${CC} --prefix=${HOME}/temp
-make
-make install
-example/sample-query
+list_output_dir () {
+        echo "====== Listing ${HOME}/test ======="
+        ls -Rl ${HOME}/test
+        echo "==================================="
+}
+
+if [ "${BUILD}" = "autotools" ]; then
+        NOCONFIGURE=1 ./autogen.sh
+        ./configure --disable-silent-rules --enable-werror CC=${CC} --prefix=${HOME}/test
+        make
+        make install
+        list_output_dir
+        example/sample-query
+elif [ "${BUILD}" = "meson" ]; then
+        mkdir build
+        cd build
+        CC=${CC} meson --prefix=${HOME}/test ..
+        ninja
+        ninja install
+        list_output_dir
+        ./sample-query
+fi
